bc182a75b6be3fe3a4acaf196e14dbc2eb10021e
rana-shahroz <ranashahrozkhan01@gmail.com>
2022-06-17 22:01:52 -0500

    D1 Plan Completed

--- sliced_opt/C++ Code/src/sliced_opt.cpp
+++ sliced_opt/C++ Code/src/sliced_opt.cpp
@@ -32,7 +32,37 @@ FourRet opt_sub(Array & M1, intArray & L1, double Lambda){
         cost_sub = cost_inact + cost_sub;
         L_sub = xt::concatenate(xtuple(L1_inact, L_sub));
         if (L_sub(i_act) == -1){
-            
+            cost_sub_pre = cost_sub;
+            return {cost_sub, L_sub, cost_sub_pre, L_sub};
         }
     }
+
+    // Find the optimal d1 plan
+    intArray L1_act = xt::view(L1, xt::range(i_act, L1.size(), 1), xt::all());
+    intArray L1x_act = xt::arange<int32_t>(i_act, n1);
+
+    intArray tmp = xt::view(L1x_act, xt::range(1, L1x_act.size(), 1), xt::all());
+    Array cost_L1 = matrix_take(M1, tmp, L1_act);
+    intArray tmp2 = xt::view(L1x_act, xt::range(0, L1x_act.size() - 1, 1), xt::all());
+    Array cost_L2 = matrix_take(M1, tmp2, L1_act);
+
+    int s = cost_L2.shape(0);
+
+    Array cost_list = xt::concatenate(xtuple(cost_L1, cost_L2));
+    Array cost_d1 = xt::zeros<T>({s+1});
+
+    int i = s;
+    for (; i > -1; --i){
+        Array tmp3 = xt::view(cost_list, xt::range(i, i + s, 1), xt::all());
+        cost_d1(i) = xt::sum(tmp3)[0];
+
+        if(i-1 >= 0 && cost_list(i-1) >= Lambda){
+            cost_d1 = xt::view(cost_d1, xt::range(i, cost_d1.size(), 1), xt::all());
+            break;
+        }
+    }
+
+    int index_d1_opt = xt::argmin(cost_d1)[0] + i;
+    double cost_d1_opt = xt::amin(cost_d1)[0] + Lambda;
+    
 }
